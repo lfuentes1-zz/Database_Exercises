c521d50d021550c498fd707ad30ce4a77e6f5c7c
Letty Fuentes <letty@MacBook-Pro.local>
2015-11-19 20:48:00 -0600

    MYSQL Tables

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist_name VARCHAR(50) NOT NULL,
+    record_name VARCHAR(50) NOT NULL,
+    release_date YEAR(4) DEFAULT 0000,
+    records_sold FLOAT DEFAULT 0,
+    record_genre VARCHAR(50) DEFAULT 'NONE',
+    PRIMARY KEY (id)
+);
