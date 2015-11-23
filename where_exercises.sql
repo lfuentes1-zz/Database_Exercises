05638c7ac2a39896a3ae5533f2f4e75ecef23c70
Letty Fuentes <letty@MacBook-Pro.local>
2015-11-23 13:18:53 -0600

    Query example using the BETWEEN

--- where_exercises.sql
+++ where_exercises.sql
@@ -5,3 +5,7 @@ WHERE first_name IN ('Irena', "Vidya", 'Maya');
 SELECT *
 FROM employees
 WHERE last_name LIKE 'E%';
+
+SELECT *
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
