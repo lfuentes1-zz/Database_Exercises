ffcf13fac74d23eda3768b7a938bec1b122b3fc0
Letty Fuentes <letty@MacBook-Pro.local>
2015-11-23 13:57:12 -0600

    Updated query to include employees who where born on Christmas.

--- where_exercises.sql
+++ where_exercises.sql
@@ -17,7 +17,8 @@ AND last_name LIKE '%E';
 
 SELECT *
 FROM employees
-WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+AND birth_date LIKE '%-12-25';
 
 SELECT *
 FROM employees
