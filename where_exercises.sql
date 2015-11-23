986e5f5a36a585a1888cdf2a1b5f3902a70be60d
Letty Fuentes <letty@MacBook-Pro.local>
2015-11-23 14:13:49 -0600

    Fixed Spacing

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,24 +1,26 @@
 SELECT * 
 FROM employees
 WHERE gender = 'M'
-AND (first_name = 'Irena' 
-OR first_name = "Vidya"
-OR first_name = 'Maya');
+  AND (
+	  first_name = 'Irena' 
+   OR first_name = "Vidya"
+   OR first_name = 'Maya'
+);
 
 SELECT *
 FROM employees
 WHERE last_name LIKE 'E%'
-OR last_name LIKE '%E';
+  OR last_name LIKE '%E';
 
 SELECT *
 FROM employees
 WHERE last_name LIKE 'E%'
-AND last_name LIKE '%E';
+  AND last_name LIKE '%E';
 
 SELECT *
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
-AND birth_date LIKE '%-12-25';
+  AND birth_date LIKE '%-12-25';
 
 SELECT *
 FROM employees
@@ -27,4 +29,4 @@ WHERE birth_date LIKE '%-12-25';
 SELECT *
 FROM employees
 WHERE last_name LIKE '%q%'
-AND last_name NOT LIKE '%qu%';
+  AND last_name NOT LIKE '%qu%';
