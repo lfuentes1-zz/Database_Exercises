2f7040d8d63dc69329a649bc811132a5496d86fd
Letty Fuentes <letty@MacBook-Pro.local>
2015-11-23 13:52:58 -0600

    Duplicated previous query and changed OR to AND

--- where_exercises.sql
+++ where_exercises.sql
@@ -12,6 +12,11 @@ OR last_name LIKE '%E';
 
 SELECT *
 FROM employees
+WHERE last_name LIKE 'E%'
+AND last_name LIKE '%E';
+
+SELECT *
+FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
 
 SELECT *
