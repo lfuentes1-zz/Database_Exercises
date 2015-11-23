e4c3b5039a3af5a2202be51b215f94fc34d057f8
Letty Fuentes <letty@MacBook-Pro.local>
2015-11-23 14:55:25 -0600

    Modified query to USE ASC and DESC

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -19,8 +19,15 @@ ORDER BY emp_no DESC;
 
 SELECT *
 FROM employees
+WHERE first_name = 'Khun'
+AND last_name = 'Bernini';
+
+SELECT *
+FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
-AND birth_date LIKE '%-12-25';
+AND birth_date LIKE '%-12-25'
+ORDER BY birth_date ASC, hire_date DESC;
+-- Khun Bernin should be the result
 
 SELECT *
 FROM employees
