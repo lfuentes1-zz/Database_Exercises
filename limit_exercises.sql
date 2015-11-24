872a13fe664aceef778e983957a290ca4061ff0b
Letty Fuentes <letty@MacBook-Pro.local>
2015-11-24 09:40:35 -0600

    Query example using LIMIT

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -2,3 +2,10 @@ SELECT last_name
 FROM employees
 GROUP BY last_name DESC
 LIMIT 10;
+
+SELECT first_name, last_name
+FROM employees
+WHERE hire_date LIKE '199%'
+AND birth_date LIKE '%-12-25'
+ORDER BY birth_date ASC, hire_date DESC
+LIMIT 5;	
