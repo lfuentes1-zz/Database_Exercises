6aaddc51b41060e656b6ca3de7dd69bda453f889
Letty Fuentes <letty@MacBook-Pro.local>
2015-11-24 13:34:42 -0600

    Update queries

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -7,7 +7,7 @@ WHERE (
 )
 GROUP BY gender;
 
-SELECT *, CONCAT(first_name, ' ' ,last_name)
+SELECT *, CONCAT(last_name, ', ' ,first_name) AS full_name
 FROM employees
 WHERE last_name LIKE 'E%'
 OR last_name LIKE '%E';
@@ -18,7 +18,9 @@ WHERE hire_date LIKE '199%'
 AND birth_date LIKE '%-12-25'
 ORDER BY hire_date ASC, birth_date DESC;
 
-SELECT *
+SELECT *, COUNT(*)
 FROM employees
 WHERE last_name LIKE '%q%'
-AND last_name NOT LIKE '%qu%';
+AND last_name NOT LIKE '%qu%'
+GROUP BY first_name, last_name
+ORDER BY first_name, last_name;
