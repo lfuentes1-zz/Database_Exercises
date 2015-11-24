299bdc08a2793868db4df9ef6a3ab288b2f1f93c
Letty Fuentes <letty@MacBook-Pro.local>
2015-11-24 11:57:22 -0600

    Removed old queries unrelated to functions

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -12,28 +12,11 @@ FROM employees
 WHERE last_name LIKE 'E%'
 OR last_name LIKE '%E';
 
-
-SELECT *
-FROM employees
-WHERE last_name LIKE 'E%'
-AND last_name LIKE '%E'
-ORDER BY emp_no DESC;
-
-SELECT *
-FROM employees
-WHERE first_name = 'Khun'
-AND last_name = 'Bernini';
-
 SELECT *, DATEDIFF(NOW(), hire_date) AS total_days
 FROM employees
 WHERE hire_date LIKE '199%'
 AND birth_date LIKE '%-12-25'
 ORDER BY hire_date ASC, birth_date DESC;
-
-
-SELECT *
-FROM employees
-WHERE birth_date LIKE '%-12-25';
 
 SELECT *
 FROM employees
